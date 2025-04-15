673b963d640dcf5872217a38fce98bb21a8ed272
Md.Shimul Khan <shimulssk.121@gmail.com>
2025-04-15 17:47:52 +0600

    Update home.jsx

--- src/Page/home.jsx
+++ src/Page/home.jsx
@@ -6,7 +6,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      <h1>HOME</h1>
+      <h1 className='bg'>HOME</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicingquas amet velit in ad veritatis dignissimos? Libero
          soluta consectetur doloribus, nulla repellat ab doloremque molestiae quisquam.
